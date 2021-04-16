3cb9c920a349bb89b1b34d6804ed5f3df27dca5d
Ole Christian Helset <ochelset@gmail.com>
2021-04-16 16:08:36 +0200

    Initial commit with animated circles being generated as the mouse is being moved

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+
+import './App.scss';
+
+interface Bubble {
+  x: number;
+  y: number;
+  color: string;
+  index: number;
+}
+
+const initialState: Bubble[] = [];
+const DELTA = 12;
+const MAX_BUBBLES = 200;
 
 function App() {
+  const [bubbles, setBubbles] = useState(initialState);
+
+  useEffect(() => {
+    setBubbles(initialState);
+  }, []);
+
+  const handleMouseMove = (event: MouseEvent): void => {
+    if (event.movementX > DELTA || event.movementY > DELTA) {
+      const newBubbles: Bubble[] = [...bubbles];
+      if (newBubbles.length >= MAX_BUBBLES) {
+        newBubbles.shift();
+      }
+
+      newBubbles.push({
+        x: event.clientX,
+        y: event.clientY,
+        color: randomColor(),
+        index: Math.floor(Math.random() * 100000)
+      });
+
+      setBubbles(newBubbles);
+    }
+  };
+
+  const randomColor = (): string => {
+    const colors = [
+      'rgba(255,0,0, 1)',
+      'rgba(0,255,0, 1)',
+      'rgba(0,0,255, 1)',
+      'rgba(255,255,0, 1)',
+      'rgba(255,0,255, 1)',
+      'rgba(0,255,255, 1)'
+    ];
+    const index = Math.floor(Math.random() * colors.length);
+
+    return colors[index];
+  };
+
+  const handleBubbleDone = (element: any): void => {
+    const newBubbles = bubbles.filter(
+      (bubble) => bubble.index.toString() !== element.id
+    );
+    setBubbles(newBubbles);
+  };
+
+  const renderBubbles = () =>
+    bubbles.map((bubble) => (
+      <div
+        id={bubble.index.toString()}
+        key={bubble.index}
+        className={'bubble'}
+        style={{
+          left: bubble.x + 'px',
+          top: bubble.y + 'px',
+          backgroundColor: bubble.color
+        }}
+      />
+    ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      onMouseMove={(event) => handleMouseMove(event.nativeEvent)}
+      onAnimationEnd={(event) => handleBubbleDone(event.nativeEvent.target)}
+    >
+      <main>
+        <span>Bubbles: {bubbles.length}</span>
+        {renderBubbles()}
+      </main>
     </div>
   );
 }
